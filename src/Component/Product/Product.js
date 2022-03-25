4ec9c8965caafd3e14ea6cc6d7591a17eeebd2fb
Niloy-Dey <niloydeyce@gmail.com>
2022-03-26 01:08:47 +0600

    react font aewsome install  finish

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -4,10 +4,13 @@ const Product = (props) => {
     // console.log(props.product);
     const {name, picture, price} = props.product;
         return (
-        <div>
-            <img className='card-picture' src={picture} alt="" />
+        <div className='card'>
+           <div className="card-info">
+           <img className='card-picture' src={picture} alt="" />
             <h5>Name : {name} </h5>
             <h5>Price : ${price} </h5>
+           </div>
+            <button className='cart-btn'>Add to cart </button>
         </div>
     );
 };
--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -5,16 +5,16 @@ import './Products.css'
 const Products = (props) => {
     const products = props.data;
     return (
-        <div>
-            <h1>Welcome to laptop world</h1>
-        <div className='product-container'>
-            {
-               products.map( pd  => <Product product={pd} key={pd.id}> </Product>)
-            }
-        </div>
-        <div className="cart-container">
-            
-        </div>
+        <div className='full-container'>
+            <h1 style={{textAlign:'center'}}>Welcome to laptop world</h1>
+            <div className='product-container'>
+                {
+                products.map( pd  => <Product product={pd} key={pd.id}> </Product>)
+                }
+            </div>
+            <div className="cart-container">
+                
+            </div>
         </div>
     );
 };
