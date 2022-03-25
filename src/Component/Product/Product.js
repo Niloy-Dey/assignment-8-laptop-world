080b3db8b42e79b2ae8ea63621d5f30ff5737d59
Niloy-Dey <niloydeyce@gmail.com>
2022-03-26 01:26:39 +0600

    responsiveness work is done

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+
 const Product = (props) => {
     // console.log(props.product);
     const {name, picture, price} = props.product;
@@ -10,7 +14,10 @@ const Product = (props) => {
             <h5>Name : {name} </h5>
             <h5>Price : ${price} </h5>
            </div>
-            <button className='cart-btn'>Add to cart </button>
+            <button className='cart-btn'>
+             <p>Add to cart</p> 
+             <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
